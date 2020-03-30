9304453269da09fb1e87f44f8a7ba75a13f11d35
Gabriele Comoretto <gcomoretto@lsst.org>
2020-03-30 16:53:16 -0700

    Fixed Capitalization and appendix justification

--- docsteady/tplan.py
+++ docsteady/tplan.py
@@ -101,15 +101,13 @@ class TestPlan(Schema):
 
         # Derived fields
         # Extract milestone information
-        sname = data['name'].split(" ")
-        namekey = sname[0][:3]
-        namekey1 = sname[0][:7]
-        if namekey == '503' or namekey1 == 'LDM-503':
-            data['milestone_id'] = sname[0]
-            data['milestone_name'] = data['name'].replace(sname[0], '').strip()
-        else:
+        sname = data['name'].split(":")
+        if len(sname) == 1:
             data['milestone_id'] = data['key']
-            data['milestone_name'] = data['name'].title()
+            data['milestone_name'] = data['name'].strip()
+        else:
+            data['milestone_id'] = sname[0]
+            data['milestone_name'] = data['name'].replace(sname[0], '').replace(":", "").strip()
 
         # Product
         data['product'] = data['folder'].split('/')[-1]
