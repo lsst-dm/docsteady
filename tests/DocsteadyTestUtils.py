65970c1e57f7508095627d64726eb4f489779b72
William O'Mullane <womullan@lsst.org>
2023-08-27 13:34:33 -0700

    mypy tidy on tests

--- tests/DocsteadyTestUtils.py
+++ tests/DocsteadyTestUtils.py
@@ -9,7 +9,7 @@ def write_test_data(data: dict, name: str) -> None:
         json.dump(data, f)
 
 
-def read_test_data(name: str) -> None:
+def read_test_data(name: str) -> dict:
     fn = f"{ROOT}/{name}.json"
     with open(fn) as f:
         return json.load(f)
